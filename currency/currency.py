ff2859842e277b538247477e38e55eae517592c1
Cris Motinha <cristinamotinham@gmail.com>
2017-04-01 14:42:10 -0300

    Adiciona arquivo de classe das moedas

---
+++ currency/currency.py
@@ -0,0 +1,9 @@
+class Currency:
+	def __init__(self, name, abbreviation):
+        self.name = name
+        self.abb = abbreviation
+        self.last_values = []
+
+    def add_value(self, value):
+        self.last_values.append(value)
+        
