286700dc156c1c7fab01551425fc638f97a224a9
Cris Motinha <cristinamotinham@gmail.com>
2017-04-01 19:25:36 -0300

    Adiciona post para recuperar dados das moedas e renderiza os gráficos certos

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask, render_template
-from currency import routes, classes
+from flask import Flask, render_template, request
+from currency import routes, classes, helpers
+from datetime import datetime
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    coin = classes.Currency("teste", "AMD")
-    coin = routes.get_quote(coin)
-    quotation = coin.last_values
-    return render_template('index.html', name = coin.name , quotes=quotation)
+@app.route('/', methods=['POST'])
+def return_graphic():
+    selected_name = request.form["currency"].split("/")[0]
+    selected_abbreviation = request.form["currency"].split("/")[1]
+    selected_currency = classes.Currency(selected_name, selected_abbreviation)
+   
+    selected_currency = routes.get_quote(selected_currency)
+
+
+    dates = helpers.get_last_week(datetime.today())
+    
+    return render_template('index.html', 
+        dates = dates, currency = selected_currency)
+
+@app.route('/', methods=['GET'])
+def return_default():
+    selected_currency = classes.Currency("Real", "BRL")
+    selected_currency = routes.get_quote(selected_currency)
+
+
+    dates = helpers.get_last_week(datetime.today())
+    
+    return render_template('index.html', 
+        dates = dates, currency = selected_currency)
 
 if __name__ == "__main__":
     app.run()
