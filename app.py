ea254b900afbe6c801b046b0b4a3b4d426dc8efa
Cris Motinha <cristinamotinham@gmail.com>
2017-04-02 19:13:59 -0300

    Cria função para evitar repetição de código

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ import os
 
 app = Flask(__name__)
 
+def get_right_template(currency, dates):
+    if None in currency.last_values:
+        return render_template('error.html')
+    else:
+        return render_template('graphic.html', dates = dates, currency = currency)
+
+
 @app.route('/', methods=['POST'])
 def return_graphic():
-    #cria uma instância de Currency com as informações do request feito pelo botão
     selected_name = request.form["currency"].split("/")[0]
     selected_abbreviation = request.form["currency"].split("/")[1]
     selected_currency = classes.Currency(selected_name, selected_abbreviation)
@@ -15,23 +21,17 @@ def return_graphic():
 
     dates = helpers.get_last_week(datetime.today()) #pega a data dos últimos 7 dias
 
-    if None in selected_currency.last_values:
-        return render_template('error.html')
-    else:
-        return render_template('graphic.html', dates = dates, currency = selected_currency)
+    return get_right_template(selected_currency, dates)
+    
 
 @app.route('/', methods=['GET'])
 def return_default():
-     #cria uma instância de Currency default, para exibição no primeiro carregamento da página
     selected_currency = classes.Currency("Real", "BRL")
     selected_currency = routes.get_quote(selected_currency)
     
     dates = helpers.get_last_week(datetime.today())
     
-    if None in selected_currency.last_values:
-        return render_template('error.html')
-    else:
-        return render_template('graphic.html', dates = dates, currency = selected_currency)
+    return get_right_template(selected_currency, dates)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
