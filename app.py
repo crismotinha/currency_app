3afee7036538b88c1c99bca8e99449c8b1510750
Cris Motinha <cristinamotinham@gmail.com>
2017-04-02 13:33:46 -0300

    Troca porta em app.py

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def return_default():
         dates = dates, currency = selected_currency)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
